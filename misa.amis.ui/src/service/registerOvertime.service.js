0749c5476e5a0538e1d37516ea705ea62e35f4c0
Đạt Nguyễn Quốc <datnq1210@gmail.com>
2021-03-07 22:57:23 +0700

    edit data

--- misa.amis.ui/src/service/registerOvertime.service.js
+++ misa.amis.ui/src/service/registerOvertime.service.js
@@ -1,131 +1,131 @@
 const registerOvertimes =
     [
-        {
-            "ID": "6044ca03a3e185205896f23b",
-            "approvedBy": "Shepherd Cruz",
-            "dateCreate": "2020-10-05",
-            "dateWorkEnd": "2021-03-02",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca0345a03f68db55a9ee",
-            "approvedBy": "Bryan Park",
-            "dateCreate": "2020-12-01",
-            "dateWorkEnd": "2020-04-19",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03d2ef7aa8a113a7a9",
-            "approvedBy": "Michele Roth",
-            "dateCreate": "2020-10-27",
-            "dateWorkEnd": "2020-11-11",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03e1c363ffdf066c4d",
-            "approvedBy": "Duncan Hartman",
-            "dateCreate": "2020-03-10",
-            "dateWorkEnd": "2020-04-11",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca033b41c9a5ccd2c236",
-            "approvedBy": "Short Robles",
-            "dateCreate": "2020-07-08",
-            "dateWorkEnd": "2020-11-02",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca0352834770f09fdfb6",
-            "approvedBy": "Gordon Mcneil",
-            "dateCreate": "2020-10-03",
-            "dateWorkEnd": "2021-02-14",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03b97673fd7a239a65",
-            "approvedBy": "Mann Harvey",
-            "dateCreate": "2020-04-02",
-            "dateWorkEnd": "2021-01-24",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03eee2a15650f17832",
-            "approvedBy": "Bonnie Hardy",
-            "dateCreate": "2020-06-24",
-            "dateWorkEnd": "2020-03-28",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03edf5e1ebc557885c",
-            "approvedBy": "Parrish Battle",
-            "dateCreate": "2020-08-15",
-            "dateWorkEnd": "2020-04-15",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca031150337911ba432b",
-            "approvedBy": "Lawrence Leach",
-            "dateCreate": "2020-07-15",
-            "dateWorkEnd": "2021-01-24",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03219e93e730352493",
-            "approvedBy": "Baldwin Flynn",
-            "dateCreate": "2020-08-07",
-            "dateWorkEnd": "2020-05-03",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03f78b9b8f22e43317",
-            "approvedBy": "Ashley Obrien",
-            "dateCreate": "2020-11-09",
-            "dateWorkEnd": "2020-10-09",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca0339434f3323cb075f",
-            "approvedBy": "Franco Hooper",
-            "dateCreate": "2020-09-15",
-            "dateWorkEnd": "2021-01-09",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca0370bfc25be6697418",
-            "approvedBy": "Gibbs Glenn",
-            "dateCreate": "2020-09-03",
-            "dateWorkEnd": "2020-12-02",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca032ed77c0b0308e4a4",
-            "approvedBy": "Gilbert Thornton",
-            "dateCreate": "2021-01-23",
-            "dateWorkEnd": "2020-11-06",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03382e9ec1ed0d5245",
-            "approvedBy": "Humphrey Gilliam",
-            "dateCreate": "2020-03-08",
-            "dateWorkEnd": "2020-06-07",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca035893171a5afb1926",
-            "approvedBy": "Watts Hurst",
-            "dateCreate": "2020-08-06",
-            "dateWorkEnd": "2020-05-22",
-            "status": "Đã duyệt"
-        },
-        {
-            "ID": "6044ca03eef3c5ceaae2a789",
-            "approvedBy": "Elinor Beach",
-            "dateCreate": "2020-03-31",
-            "dateWorkEnd": "2020-07-01",
-            "status": "Đã duyệt"
-        }
+        // {
+        //     "ID": "6044ca03a3e185205896f23b",
+        //     "approvedBy": "Shepherd Cruz",
+        //     "dateCreate": "2020-10-05",
+        //     "dateWorkEnd": "2021-03-02",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca0345a03f68db55a9ee",
+        //     "approvedBy": "Bryan Park",
+        //     "dateCreate": "2020-12-01",
+        //     "dateWorkEnd": "2020-04-19",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03d2ef7aa8a113a7a9",
+        //     "approvedBy": "Michele Roth",
+        //     "dateCreate": "2020-10-27",
+        //     "dateWorkEnd": "2020-11-11",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03e1c363ffdf066c4d",
+        //     "approvedBy": "Duncan Hartman",
+        //     "dateCreate": "2020-03-10",
+        //     "dateWorkEnd": "2020-04-11",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca033b41c9a5ccd2c236",
+        //     "approvedBy": "Short Robles",
+        //     "dateCreate": "2020-07-08",
+        //     "dateWorkEnd": "2020-11-02",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca0352834770f09fdfb6",
+        //     "approvedBy": "Gordon Mcneil",
+        //     "dateCreate": "2020-10-03",
+        //     "dateWorkEnd": "2021-02-14",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03b97673fd7a239a65",
+        //     "approvedBy": "Mann Harvey",
+        //     "dateCreate": "2020-04-02",
+        //     "dateWorkEnd": "2021-01-24",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03eee2a15650f17832",
+        //     "approvedBy": "Bonnie Hardy",
+        //     "dateCreate": "2020-06-24",
+        //     "dateWorkEnd": "2020-03-28",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03edf5e1ebc557885c",
+        //     "approvedBy": "Parrish Battle",
+        //     "dateCreate": "2020-08-15",
+        //     "dateWorkEnd": "2020-04-15",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca031150337911ba432b",
+        //     "approvedBy": "Lawrence Leach",
+        //     "dateCreate": "2020-07-15",
+        //     "dateWorkEnd": "2021-01-24",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03219e93e730352493",
+        //     "approvedBy": "Baldwin Flynn",
+        //     "dateCreate": "2020-08-07",
+        //     "dateWorkEnd": "2020-05-03",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03f78b9b8f22e43317",
+        //     "approvedBy": "Ashley Obrien",
+        //     "dateCreate": "2020-11-09",
+        //     "dateWorkEnd": "2020-10-09",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca0339434f3323cb075f",
+        //     "approvedBy": "Franco Hooper",
+        //     "dateCreate": "2020-09-15",
+        //     "dateWorkEnd": "2021-01-09",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca0370bfc25be6697418",
+        //     "approvedBy": "Gibbs Glenn",
+        //     "dateCreate": "2020-09-03",
+        //     "dateWorkEnd": "2020-12-02",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca032ed77c0b0308e4a4",
+        //     "approvedBy": "Gilbert Thornton",
+        //     "dateCreate": "2021-01-23",
+        //     "dateWorkEnd": "2020-11-06",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03382e9ec1ed0d5245",
+        //     "approvedBy": "Humphrey Gilliam",
+        //     "dateCreate": "2020-03-08",
+        //     "dateWorkEnd": "2020-06-07",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca035893171a5afb1926",
+        //     "approvedBy": "Watts Hurst",
+        //     "dateCreate": "2020-08-06",
+        //     "dateWorkEnd": "2020-05-22",
+        //     "status": "Đã duyệt"
+        // },
+        // {
+        //     "ID": "6044ca03eef3c5ceaae2a789",
+        //     "approvedBy": "Elinor Beach",
+        //     "dateCreate": "2020-03-31",
+        //     "dateWorkEnd": "2020-07-01",
+        //     "status": "Đã duyệt"
+        // }
     ]
 
 
